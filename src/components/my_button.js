551252c6e77d21c4d62d8fc88da009915ccbe277
unknown <Digital@DH-A10-06.dhbrasil.digitalhouse.com>
2020-02-07 12:08:59 -0300

    Lifecycle com React

--- src/components/my_button.js
+++ src/components/my_button.js
@@ -7,7 +7,6 @@ export default class MyButton extends React.Component {
         this.state = {
             mensagem_secreta: [80, 97, 108, 109, 101, 105, 114, 97, 115, 32, 110, 227, 111, 32, 116, 101, 109, 32, 109, 117, 110, 100, 105, 97, 108, 33]
         }
-
         // Linha necessária para poder usar o "this" 
         // dentro de uma função que foi passada como callback
         this.lidarComClick = this.lidarComClick.bind(this);
@@ -15,10 +14,6 @@ export default class MyButton extends React.Component {
 
     lidarComClick(e) {
         this.props.onMyButtonClick(this.state.mensagem_secreta);
-    }
-
-    encrpyt() {
-
     }
 
     render() {
